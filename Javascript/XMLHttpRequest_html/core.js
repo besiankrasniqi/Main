c54ffb87a2fde23fda11a8c80d895b3a4cbb0a69
= <portfoliobk@gmail.com>
2015-02-07 19:10:04 -0500

    XMLHttpRequest_html loading HTML content through Javascript

---
+++ Javascript/XMLHttpRequest_html/core.js
@@ -0,0 +1,30 @@
+/**
+* Created by Besian Krasniqi on 02/07/14
+* email: besian@bksofteng.com
+* website: www.bksofteng.com
+*/
+
+//Retrieving a json file using pure JavaScript
+var mainContainer = document.getElementById("main");
+var url = "content.html";
+
+var oReq = new XMLHttpRequest();
+oReq.open("get", url, true);
+oReq.onload = showJson;
+oReq.send();
+
+
+function showJson() { 
+	if (oReq.readyState == 4 ) {
+		if (oReq.status >= 200 || oReq.staus <= 300 || oReq.status == 304) {
+			var getFile = oReq.responseText; 
+			mainContainer.innerHTML = getFile;
+		}
+	}
+	else {
+		mainContainer.innerHTML = "The file " + url + " could not be loaded";
+	}
+
+
+} 
+
